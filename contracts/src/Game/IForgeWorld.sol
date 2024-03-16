dbc2c818cb0454c0a5321ac96b187451e87792d5
Pri Patel <pripatel93@gmail.com>
2024-03-16 15:03:40 +0000

    add interface IForgeWorld

---
+++ contracts/src/Game/IForgeWorld.sol
@@ -0,0 +1,29 @@
+// cast interface -n IForgeWorld ./out/ForgeWorld.sol/ForgeWorld.json
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IForgeWorld {
+    event EpochIncreased(uint256 newEpoch);
+    event UserClaimedRewards(address indexed user, uint256 epoch, address resourceAddress, uint256 reward);
+    event UserJoinedWorld(address indexed user, uint256 world);
+    event UserMovedWorld(address indexed user, uint256 fromWorld, uint256 toWorld);
+    event WorldDeployed(uint256 indexed worldId, address tokenResource, string name, string symbol);
+
+    function abilities(uint256) external view returns (uint256);
+    function abilityCounter() external view returns (uint256);
+    function cumulativeResourceCollectedPerEpoch(uint256, address) external view returns (uint256);
+    function epoch() external view returns (uint256);
+    function increaseEpoch() external;
+    function lastEpochTimestamp() external view returns (uint256);
+    function userAbilities(address, uint256) external view returns (uint256);
+    function userClaimRewards() external;
+    function userCurrentWorld(address) external view returns (uint256);
+    function userJoinWorld(uint256 world) external;
+    function userLastEpochClaimed(address) external view returns (uint256);
+    function userLevelUpAbility(uint256 ability) external;
+    function userMoveWorld(uint256 world) external;
+    function worldCounter() external view returns (uint256);
+    function worldPopulation(uint256) external view returns (uint256);
+    function worldToTokenResource(uint256) external view returns (address);
+    function worlds(uint256) external view returns (uint256);
+}
