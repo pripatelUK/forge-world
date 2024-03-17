c3cc4811410d551e71e63a770da967ac5e5aaa8d
Pri Patel <pripatel93@gmail.com>
2024-03-17 00:16:50 +0000

    fix mobile bug, WIP

--- mobile/App.js
+++ mobile/App.js
@@ -4,30 +4,6 @@ import '@ethersproject/shims';
 import process from 'process';
 global.process = process;
 import { WagmiConfig } from 'wagmi'
-import { createWeb3Modal, defaultWagmiConfig, Web3Modal, W3mButton } from '@web3modal/wagmi-react-native'
-
-// import { GameContextProvider } from "./contexts/GameContext";
-
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import NfcManager, { NfcTech } from 'react-native-nfc-manager';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useState } from 'react';
-
-import { HomeScreen } from './components/HomeScreen';
-// import { CreateAccount } from './components/CreateAccount';
-// import { WorldsScreen } from './components/WorldsScreen';
-// import { ProfileScreen } from "./components/ProfileScreen";
-// import MainMenuScreen from "./screens/MainMenuScreen";
-// import { CharacterSelectScreen } from "./screens/CharacterSelectScreen";
-// import { GameScreen } from "./screens/GameScreen";
 import {
   arbitrum,
   mainnet,
@@ -42,6 +18,33 @@ import {
   celo,
   aurora,
 } from 'wagmi/chains';
+import { createWeb3Modal, defaultWagmiConfig, Web3Modal, W3mButton } from '@web3modal/wagmi-react-native'
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+import { FlexView, Text } from '@web3modal/ui-react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+
+// import { HomeScreen } from './components/HomeScreen';
+// import { CreateAccount } from './components/CreateAccount';
+// import { CustomerTransactionScreen } from './components/CustomerTransactionScreen';
+import { GameContextProvider } from "./contexts/GameContext";
+import { HomeScreen } from './components/HomeScreen';
+import { CreateAccount } from './components/CreateAccount';
+import { WorldsScreen } from './components/WorldsScreen';
+import { ProfileScreen } from "./components/ProfileScreen";
+import { MainMenuScreen } from "./screens/MainMenuScreen";
+import { CharacterSelectScreen } from "./screens/CharacterSelectScreen";
+import { GameScreen } from "./screens/GameScreen";
+
 const Stack = createStackNavigator();
 
 // 1. Get projectId
@@ -49,25 +52,41 @@ const projectId = '49a082ffca38748d2ef8acceca12a92e'
 
 // 2. Create config
 const metadata = {
-  name: 'ForgeWorld',
-  description: 'ForgeWorld',
-  url: 'https://ForgeWorld.xyz',
+  name: 'CrossPay',
+  description: 'CrossPay',
+  url: 'https://crosspay.xyz',
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
   redirect: {
     native: 'crosspay://',
     // universal: 'YOUR_APP_UNIVERSAL_LINK.com'
   },
 };
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+
 const chains = [
   mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora,
 ];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
 // 3. Create modal
 createWeb3Modal({
   projectId,
   chains,
   wagmiConfig
 })
+
 // initialize NfcManager on application's startup
 NfcManager.start();
 
@@ -96,23 +115,27 @@ export default function App() {
 
 
   return (
-    < NavigationContainer >
-      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
-      <Stack.Navigator
-        initialRouteName={sessionToken ? 'Home' : 'CreateAccount'}
-        screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="CreateVirtual" component={CreateAccount} /> */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        {/* <Stack.Screen name="ArxRegister" component={ArxRegisterScreen} /> */}
-        {/* <Stack.Screen name="Worlds" component={WorldsScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name={"MainMenu"} component={MainMenuScreen} />
-          <Stack.Screen
-            name={"CharacterSelect"}
-            component={CharacterSelectScreen}
-          />
-          <Stack.Screen name={"Game"} component={GameScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer >
+    <WagmiConfig config={wagmiConfig}>
+      <GameContextProvider>
+        <NavigationContainer>
+          <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
+          <Stack.Navigator
+            initialRouteName={sessionToken ? 'Home' : 'CharacterSelectScreen'}
+            screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="CreateVirtual" component={CharacterSelectScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            {/* <Stack.Screen name="ArxRegister" component={ArxRegisterScreen} /> */}
+            <Stack.Screen name="Worlds" component={WorldsScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name={"MainMenu"} component={MainMenuScreen} />
+            <Stack.Screen
+              name={"CharacterSelect"}
+              component={CharacterSelectScreen}
+            />
+            <Stack.Screen name={"Game"} component={GameScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GameContextProvider>
+    </WagmiConfig>
   )
 }
